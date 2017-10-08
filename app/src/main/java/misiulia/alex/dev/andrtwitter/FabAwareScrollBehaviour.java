c9e0af5c776e40ee4a916921bf7fe143da49cb71
A.Misiuilia <a.misiulia@ezrapplication.com>
2017-10-08 20:26:19 +0300

    [ADD] FAB to UserInfoScreen

---
+++ app/src/main/java/misiulia/alex/dev/andrtwitter/FabAwareScrollBehaviour.java
@@ -0,0 +1,40 @@
+package misiulia.alex.dev.andrtwitter;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FabAwareScrollBehaviour extends CoordinatorLayout.Behavior<FloatingActionButton> {
+    public FabAwareScrollBehaviour(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
+                                       final View directTargetChild, final View target, final int nestedScrollAxes) {
+        return true;
+    }
+
+    @Override
+    public void onNestedScroll(final CoordinatorLayout coordinatorLayout,
+                               final FloatingActionButton child,
+                               final View target, final int dxConsumed, final int dyConsumed,
+                               final int dxUnconsumed, final int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onHidden(fab);
+                    // need because gone views are not receiving scroll events
+                    // https://stackoverflow.com/questions/41761736/android-design-library-25-1-0-causes-floatingactionbutton-behavior-to-stop-worki/41811364#41811364
+                    child.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+}
