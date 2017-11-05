6cfc12d7c02fd0604b164289752e683c65463e12
A.Misiuilia <a.misiulia@ezrapplication.com>
2017-11-05 14:26:50 +0300

    [REF] move logic of handling home button in BaseActivity

--- app/src/main/java/misiulia/alex/dev/andrtwitter/BaseActivity.java
+++ app/src/main/java/misiulia/alex/dev/andrtwitter/BaseActivity.java
@@ -1,6 +1,18 @@
 package misiulia.alex.dev.andrtwitter;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return(super.onOptionsItemSelected(item));
+    }
 }
