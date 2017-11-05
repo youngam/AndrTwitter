6e9acc66bbacad6c2687d92f201dcad56aa91cd1
A.Misiuilia <a.misiulia@ezrapplication.com>
2017-11-05 13:57:33 +0300

    [REF] fonts to new android api, remove caligraphy depdendency

--- app/src/main/java/misiulia/alex/dev/andrtwitter/BaseActivity.java
+++ app/src/main/java/misiulia/alex/dev/andrtwitter/BaseActivity.java
@@ -1,15 +1,6 @@
 package misiulia.alex.dev.andrtwitter;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
-
 public class BaseActivity extends AppCompatActivity {
-
-    // need for calligraphy library usage
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(base));
-    }
 }
--- app/src/main/java/misiulia/alex/dev/andrtwitter/TwitterApp.java
+++ app/src/main/java/misiulia/alex/dev/andrtwitter/TwitterApp.java
@@ -1,11 +1,8 @@
 package misiulia.alex.dev.andrtwitter;
 
 import android.app.Application;
-import android.content.Context;
-
 import com.twitter.sdk.android.core.Twitter;
 
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class TwitterApp extends Application{
 
